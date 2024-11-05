c3561ecd93110fc8cc6affbc63798b69e6375076
Zhuoli Huang <2915831851@qq.com>
2024-11-05 14:43:32 +0800

    Homework3

---
+++ Homework3/zlihuang/src/App.vue
@@ -0,0 +1,73 @@
+<script lang="ts">
+
+import Notes from './components/Notes.vue'
+import NotesWithStore from './components/NotesWithStore.vue'
+import BarChart from './components/BarChart.vue'
+import ScatterPlot from './components/ScatterPlot.vue'
+import SankeyChart from './components/SankeyChart.vue'
+interface BarHoverData {
+    type: 'Dalc' | 'Walc';
+    level: string;
+}
+export default {
+  components: {
+    Notes,
+    NotesWithStore,
+    BarChart,
+    ScatterPlot,
+    SankeyChart,
+  },
+  data() {
+    return {
+      filteredScatterData: undefined as BarHoverData | undefined,
+      highlightedNode: undefined as BarHoverData | undefined
+    };
+  },
+  methods: {
+    updateData(data: BarHoverData) {
+      this.filteredScatterData = data;
+      this.highlightedNode = data;
+    },
+  }
+}
+</script>
+
+<template>
+  <v-container id="main-container" class="d-flex flex-column justify-space-between">
+    <v-row no-gutters class="flex-grow-1" style="height: 65%;position: relative;">
+      <v-col cols="6" class="d-flex" >
+        <BarChart @bar-hover="updateData" />
+        <textarea style="position: absolute; bottom:20px; left: 0; width: 100%;height: 220px;">Explanation:
+1.Hang on the bar, the scatter will  delete other scatter of
+the same type, the sankey will highlight relevant node and links with animation
+
+2. Hang on the bar,it will show the amount.Hang on the scatter point, it will 
+show the x-value,y-value and how many people(count).In sankeyChart,hang 
+on the nodes, it will highlight relevant links and hang on the links, it will 
+show the Percentage of a single link to two adjacent nodes.
+        </textarea>
+      </v-col>
+      <v-col cols="6" class="d-flex">
+        <ScatterPlot :filteredData="filteredScatterData"/>
+      </v-col>
+    </v-row>
+    <v-row no-gutters class="flex-grow-1" style="height: 35%;">
+        <v-col cols="12">
+            <SankeyChart :highlightedNode="highlightedNode"/>
+        </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+#main-container {
+    height: 100vh;
+    overflow: hidden;
+}
+
+.v-row > .v-col {
+    height: 100%; 
+}
+
+
+</style>
